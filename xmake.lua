36f83684a7d757b1a4933b1f67430c40fc8747fb
realzvqle <realzvqle@gmail.com>
2025-04-27 12:59:59 -0500

    s

--- xmake.lua
+++ xmake.lua
@@ -10,9 +10,7 @@ target("editor")
         add_linkdirs("lib/windows") 
         add_links("raylibdll", "raylib") 
         add_syslinks("gdi32", "winmm") 
-        add_ldflags("/fsanitize=address");
     else
         add_linkdirs("lib/linux") 
         add_links("libraylib.a") 
-        add_ldflags("-fsanitize=address");
     end
